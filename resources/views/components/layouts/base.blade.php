1f47e64cc6df8845c02f62c5cc590bd841d89bae
Nkevin03 <55061705+Nkevin03@users.noreply.github.com>
2023-07-25 14:35:19 +0200

    base og

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -29,7 +29,7 @@
     
     <meta property="og:image" content="https://feralpisalo.demo.misterketing.it/storage/{{ $page->immagine_evidenza }}" />
     <meta property="og:title" content="{{ $page->title }}" />
-    <meta property="og:title" content="{{ $page->meta }}" />
+    <meta property="og:description" content="{{ $page->meta }}" />
     <meta property="og:url" content="{{ $page->slug }}" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
