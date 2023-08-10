79991608ec2d227b5309a44f0337f3b923a1096c
Nkevin03 <55061705+Nkevin03@users.noreply.github.com>
2023-08-10 11:57:23 +0200

    base

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -17,7 +17,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    
     <meta property="og:image" content="https://feralpisalo.demo.misterketing.it/storage/{{ $immagine_evidenza }}" />
     <meta property="og:title" content="{{ $title }} | Feralpisalò" />
     <meta property="og:description" content="{{ $meta }}" />
