5b6faa73bbfdd2ef74c786b191edf8687043bb7f
ziovanja <mail@ziovanja.com>
2025-07-18 09:29:21 +0200

    Fix

--- src/Resources/PageResource.php
+++ src/Resources/PageResource.php
@@ -195,7 +195,7 @@ class PageResource extends Resource
                     ->toggleable()
                     ->getStateUsing(fn (?PageContract $record) => FilamentFabricator::getPageUrlFromId($record->id) ?: null)
                     ->url(fn (?PageContract $record) => FilamentFabricator::getPageUrlFromId($record->id) ?: null, true)
-                    ->visible(config('filament-fabricator.routing.enabled')),
+                    ->visible(config('filament-fabricator.routing.enabled') ?? true),
 
                 TextColumn::make('categoria'),
 
@@ -219,7 +219,7 @@ class PageResource extends Resource
                     ->icon('heroicon-o-external-link')
                     ->openUrlInNewTab()
                     ->color('success')
-                    ->visible(config('filament-fabricator.routing.enabled')),
+                    ->visible(config('filament-fabricator.routing.enabled') ?? true),
             ])
             ->bulkActions([
                 DeleteBulkAction::make()
